a07d307dfd346e95a78ce82e4ce2b5e339088ceb
Irfan Ali <114615634+Irfanaliqureshi@users.noreply.github.com>
2022-10-13 14:05:39 +0500

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,22 @@
+class variables {
+public static void main(String[] args){
+int a=1,b=2,c=3,d=4;
+System.out.println( "INTEGER VARIABLES");
+System.out.println("here a variable store integer datatype "+a);
+System.out.println("here b variable store integer datatype "+b);
+System.out.println("here c variable store integer datatype "+c);
+System.out.println("here d variable store integer datatype "+d);
+double e=1.1,f=2.2,g=3.3,h=4.4;
+System.out.println( "FLOAT VARIABLES");
+System.out.println("here e variable store float datatype "+e);
+System.out.println("here f variable store float datatype "+f);
+System.out.println("here g variable store float datatype "+g);
+System.out.println("here h variable store float datatype "+h);
+char m='A',n='B',o='C',p='D';
+System.out.println( "CHARACTER  VARIABLES");		
+System.out.println("here m variable store character datatype "+m);
+System.out.println("here n variable store character datatype "+n);
+System.out.println("here o variable store character datatype "+o);
+System.out.println("here p variable store character datatype "+p);
+}
+}
